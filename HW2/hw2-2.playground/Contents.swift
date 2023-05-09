cffe38b3a671eefb42809abde67acbb869e1d64d
ko4i <vladi40k@icloud.com>
2023-05-09 15:49:16 +0200

    remake

---
+++ HW2/hw2-2.playground/Contents.swift
@@ -0,0 +1,31 @@
+let months = ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"]
+
+var monthDictionary = [Int: String]()
+for (index, month) in months.enumerated() {
+            monthDictionary[index] = month
+}
+
+var calendar = [Int: [String]]()
+for year in 1970...2022{
+            calendar[year] = months
+    
+}
+
+for (year, _) in calendar {
+    calendar[year]?.append("Мій місяць")
+    var days = [Int]()
+    for day in 1...31 {
+        days.append(day)
+    }
+    var calendarPro = [Int : [String: [Int]]] ()
+    for (year, months) in calendar {
+        var monthDictionary = [String: [Int]]()
+        for month in months {
+            monthDictionary[month] = days
+        }
+        calendarPro[year] = monthDictionary
+        
+    }
+    
+}
+
