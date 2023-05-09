6197659b4d678058e37965506cb5cfdf4c798a01
ko4i <vladi40k@icloud.com>
2023-05-09 17:06:43 +0200

    123

--- HW2/SwiftPeliukhovskyiVladHW2.playground/Contents.swift
+++ HW2/SwiftPeliukhovskyiVladHW2.playground/Contents.swift
@@ -4,27 +4,28 @@ var monthDictionary = [Int: String]()
 for (index, month) in months.enumerated() {
             monthDictionary[index] = month
 }
-
+//print(monthDictionary)
 var calendar = [Int: [String]]()
 for year in 1970...2022{
             calendar[year] = months
-    
 }
+//print(calendar)
 
-for (year, _) in calendar {
-    calendar[year]?.append("Мій місяць")
-    var days = [Int]()
+//for (year, _) in calendar {
+//    calendar[year]?.append("Мій місяць")
+//}
+var days = [Int]()
     for day in 1...31 {
         days.append(day)
     }
-    var calendarPro = [Int : [String: [Int]]] ()
-    for (year, months) in calendar {
-        var monthDictionary = [String: [Int]]()
-        for month in months {
+
+var calendarPro = [Int : [String: [Int]]] ()
+for (year, months) in calendar {
+    var monthDictionary = [String: [Int]]()
+    for month in months {
             monthDictionary[month] = days
-        }
-        calendarPro[year] = monthDictionary
-        
     }
-    
+    calendarPro[year] = monthDictionary
+        
 }
+
